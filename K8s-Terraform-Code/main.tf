5c3936e6210f79f344c8f8e544c328dbda1d6bed
Akshay Mathew Joseph <83343217+akshaymaersk@users.noreply.github.com>
2021-12-14 17:38:16 +0530

    Update main.tf

--- K8s-Terraform-Code/main.tf
+++ K8s-Terraform-Code/main.tf
@@ -105,7 +105,7 @@ module "vpc" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
-  cluster_version = "1.17"
+  cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
